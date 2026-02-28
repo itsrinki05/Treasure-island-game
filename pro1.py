9d9fd51a49f6dccff59ec77ec230adfcb5f136c3
Rinki <itsrinki05@gmail.com>
2026-02-28 10:30:03 +0530

    Delete pro1.py

--- pro1.py
+++
@@ -1,6 +0,0 @@
-# A brand name genrator 
-print("Hello,welcome to the brand generator:)")
-city = input("Enter the city where you grew-up!\n")
-pet = input("enter your pet name!\n")
-print("your brand name could be:-"+city+"",pet)
-
